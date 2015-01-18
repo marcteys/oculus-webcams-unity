5a8f2036d1cc225faf25ab3f5900a8dcbc6e1e1a
Marc Teyssier <marc.teys@gmail.com>
2015-01-18 13:35:17 +0100

    cleanup some code & unity scene

--- Assets/Scripts/WebcamTexture.cs
+++ Assets/Scripts/WebcamTexture.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class WebcamTexture : MonoBehaviour {
 
-	 WebCamTexture webcamTexture;
+	WebCamTexture webcamTexture;
 	public int webcamNumber;
-	GUITexture BackgroundTexture;
 	float cameraAspect;
 
-	//fit screen
+	//fit camera plane to screen
 	private float margin = 0f;
 	public float scaleFactor = 1f;
     public bool rotatePlane = false;
@@ -19,24 +18,19 @@ public class WebcamTexture : MonoBehaviour {
 		WebCamDevice[] devices = WebCamTexture.devices;
 		webcamTexture = new WebCamTexture();
 
-		BackgroundTexture = gameObject.GetComponent<GUITexture>();
-		BackgroundTexture.pixelInset = new Rect(0,0,Screen.width,Screen.height);
-
-
 		if(devices.Length > 0)
 		{
 			webcamTexture.deviceName = devices[webcamNumber].name;
 			webcamTexture.Play();
 			this.renderer.material.mainTexture = webcamTexture;
-			BackgroundTexture.texture = webcamTexture;
-
 		}
 
         if (rotatePlane) this.transform.Rotate(Vector3.forward, 180);
 		FitScreen ();
 	}
 
-	void FitScreen() {
+	void FitScreen()
+    {
 		Camera cam = this.transform.parent.camera;
 		
 		float height = cam.orthographicSize * 2.0f;
